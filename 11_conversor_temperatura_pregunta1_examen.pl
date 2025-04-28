c38d61a289f68678b791f005b8d85673aed4d8b7
𝓙𝓔𝔅𝔘Sི༢ུ  . <109987501+JEBUSXD@users.noreply.github.com>
2025-04-28 00:40:00 -0400

    conversor
    este codigo  hace converciones de temperatura de celsius a kelvin y a fahrenheit

---
+++ 11_conversor_temperatura_pregunta1_examen.pl
@@ -0,0 +1,39 @@
+celsius(C,K,F):-atom_number(C,CC), 
+		KE is CC+273.15,
+		FA is (CC*9/5)+32,
+		send(K, selection,KE),
+		send(F, selection,FA).
+kelvin(C,K,F):-atom_number(K,KK), 
+		CE is KK-273.15,
+		FA is (KK-273.15)*9/5+32,
+		send(C, selection,CE),
+		send(F, selection,FA).
+fahrenheit(C,K,F):-atom_number(F,FF), 
+		CE is (FF-32)*5/9,
+		KE is (FF-32)*5/9+273.15,
+		send(C, selection,CE),
+		send(K, selection,KE).
+:- use_module(library(pce)).
+inicio:-new(Ventana, dialog('Calculadora Grafica.')),
+	new(Etiqueta, label(nombre,'Calculadora aritmetica basica')),
+	new(C, text_item('Celsius')),
+     	new(K, text_item('Kelvin')),
+	new(F, text_item('Fahrenheit:')),
+
+	new(CeBoton, button('Celsius', message(@prolog,
+		celsius,C?selection,K,F))),
+	new(KeBoton, button('Kelvin', message(@prolog,
+		kelvin,C,K?selection,F))),
+	new(FaBoton, button('Fahrenheit', message(@prolog,
+		fahrenheit,C,K,F?selection))),
+	new(Salir, button('salir', message(Ventana, destroy))),
+   
+	send(Ventana, append, Etiqueta),
+	send(Ventana, append, C),
+	send(Ventana, append, K),
+	send(Ventana, append, F),
+	send(Ventana, append, CeBoton),
+        send(Ventana, append, KeBoton),
+	send(Ventana, append, FaBoton),
+	send(Ventana, append, Salir),
+send(Ventana, open).
