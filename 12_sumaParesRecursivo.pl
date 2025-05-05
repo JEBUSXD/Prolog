8f6168546a6744217d2f52475f33a227c3049f86
𝓙𝓔𝔅𝔘Sི༢ུ  . <109987501+JEBUSXD@users.noreply.github.com>
2025-05-05 19:18:07 -0400

    Delete_12_sumaParesRecursivo.pl
    Se eliminó para ponerlo en un archivo aparte

--- 12_sumaParesRecursivo.pl
+++
@@ -1,8 +0,0 @@
-sumapar(N,S) :- sumapar(N, 1, 0, SU).
-sumapar(N, I, AC, SU) :-I > N, 	write(AC).
-sumapar(N, I, AC, SU) :-
-    write('Ingrese un numero: '),
-    read(X),
-    0 is X mod 2, NuevoAC is AC + X ; NuevoAC is AC,
-    II is I + 1,
-    sumapar(N, II, NuevoAC, SU).
